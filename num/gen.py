e7db7bfc90de2c5d52987c83d393f263ea0496b2
Albert Gutowski <ablikexe@gmail.com>
2014-09-14 19:51:50 +0200

    naprawiony generator do NUM

--- num/gen.py
+++ num/gen.py
@@ -1,9 +1,12 @@
 import random, os
+random.seed(0)
+TASK = 'num'
+path = lambda s: TASK + '/' + s
 r = random.randint
 T = 200
 tests = ''
 for t in xrange(T):
-    f = open('%d.in' % t, 'w')
+    f = open(path('%d.in') % t, 'w')
     for _ in xrange(10):
         if t < T-2:
             f.write('%d\n' % (r(0,100)))
@@ -12,8 +15,8 @@ for t in xrange(T):
         else:
             f.write('%d\n' % (r(int(-1e20), int(-1e19))))
     f.close()
-    os.system('python sol.py < %d.in > %d.out' % (t, t))
+    os.system('python %s < %s > %s' % (path('sol.py'), path('%d.in'%t), path('%d.out'%t)))
     tests += '%d %d.in %d.out\n' % (t, t, t)
-f = open('tests', 'w')
+f = open(path('tests'), 'w')
 f.write(tests)
 f.close()
