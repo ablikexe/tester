e1bd062e768fc2a5e718c7265f43d048b3c515b8
teqwve <teqwve@openmailbox.org>
2014-09-21 23:14:59 +0200

    basic logs

--- tester/wsgi.py
+++ tester/wsgi.py
@@ -12,3 +12,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tester.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+#logging, ponoc sie uruchamia tylko raz
+import logging
+logging.basicConfig(filename="./log", format='%(levelname)-8s %(asctime)-15s %(message)s', level=logging.INFO)
+logging.info ('starting')
